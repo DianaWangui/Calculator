b81203f0dc93f41dde41603bfea1e8f6754b8572
DianaWangui <dianawanguikt048@gmail.com>
2024-02-09 20:48:39 +0300

    update operation.c

---
+++ operation.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/*This is the function prompt that allows user to make the choice
+of the operator and enter the number they want to add*/
+
+int operation(void)
+{
+  int choice;
+  int values;
+  int count = 0;
+  int num;
+   int result;
+
+  printf("Enter your choice here: ")
+  scanf("%d", &choice);
+
+  switch(choice)
+  {
+    case 1:
+      printf("Enter your numbers here, separated by space:");
+      values = malloc((count + 1) * sizeof(int));
+      values[count++] = num;
+      count++;
+
+      /*This will check if user enters more than 5 operations*/
+      if (count > 5)
+      {
+        fprintf("Cannot take more than 5 values");
+        return (1);
+      }
+      else 
+      {
+        /*This is from the function.c file*/
+        result = add(count, num[1], num[2], num[3], num[4]);
+      }
+  }
+}
