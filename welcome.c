ae5c3d792b3cc03e44b83e6704ebf64f0854f053
DianaWangui <dianawanguikt048@gmail.com>
2024-02-09 20:48:18 +0300

    update welcome.c

--- welcome.c
+++ welcome.c
@@ -6,10 +6,12 @@ With the choic of operation to do
 
 void welcome(void)
 {
-  printf(" Welcome to our calculator project\n");
+  printf("Welcome to our calculator project\n");
   printf("Please choose 1, 2, 3, 4 for the operators as follows.\n");
   printf("1. Addition\n");
   printf("2. Subtraction\n");
   printf("3. Multiplication\n");
   printf("4. Division\n");
+  operation();
 }
+
