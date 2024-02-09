7b6951f289039fa87afd9837b118ec652d1eb64e
DianaWangui <dianawanguikt048@gmail.com>
2024-02-09 17:40:48 +0300

    update function prototypes

---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void)
+{
+  /*Main function calling other functions*/
+}
---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int add(count, ...);
+int subtract(count, ...);
+int division(count, ...);
+int multiply(count, ...);
+
+#endif *MAIN_H*
