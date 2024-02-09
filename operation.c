885d88b8408e6571a98b9cabdf51d46a40c6cd9a
DianaWangui <dianawanguikt048@gmail.com>
2024-02-09 20:52:33 +0300

    update the other operators

--- operation.c
+++ operation.c
@@ -16,7 +16,65 @@ int operation(void)
 
   switch(choice)
   {
+    /*addition*/
     case 1:
+      printf("Enter your numbers here, separated by space:");
+      values = malloc((count + 1) * sizeof(int));
+      values[count++] = num;
+      count++;
+
+      /*This will check if user enters more than 5 operations*/
+      if (count > 5)
+      {
+        fprintf("Cannot take more than 5 values");
+        return (1);
+      }
+      else 
+      {
+        /*This is from the function.c file*/
+        result = add(count, num[1], num[2], num[3], num[4]);
+      }
+
+      /*subtraction*/
+      case 2:
+      printf("Enter your numbers here, separated by space:");
+      values = malloc((count + 1) * sizeof(int));
+      values[count++] = num;
+      count++;
+
+      /*This will check if user enters more than 5 operations*/
+      if (count > 5)
+      {
+        fprintf("Cannot take more than 5 values");
+        return (1);
+      }
+      else 
+      {
+        /*This is from the function.c file*/
+        result = subtract(count, num[1], num[2], num[3], num[4]);
+      }
+
+      /*multiplication*/
+      case 3:
+      printf("Enter your numbers here, separated by space:");
+      values = malloc((count + 1) * sizeof(int));
+      values[count++] = num;
+      count++;
+
+      /*This will check if user enters more than 5 operations*/
+      if (count > 5)
+      {
+        fprintf("Cannot take more than 5 values");
+        return (1);
+      }
+      else 
+      {
+        /*This is from the function.c file*/
+        result = add(count, num[1], num[2], num[3], num[4]);
+      }
+
+      /*Division*/
+      case 4:
       printf("Enter your numbers here, separated by space:");
       values = malloc((count + 1) * sizeof(int));
       values[count++] = num;
