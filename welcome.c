7b8a64fd2ee2c8bbe989666e9d4bbb038935d14c
DianaWangui <dianawanguikt048@gmail.com>
2024-02-09 20:01:09 +0300

    update the welcome function

--- welcome.c
+++ welcome.c
@@ -1,5 +1,15 @@
-#include 'main.h'
+#include "main.h"
 
 /*This contains the welcome text and the user interaction prompt
 With the choic of operation to do 
 */
+
+void welcome(void)
+{
+  printf(" Welcome to our calculator project\n");
+  printf("Please choose 1, 2, 3, 4 for the operators as follows.\n");
+  printf("1. Addition\n");
+  printf("2. Subtraction\n");
+  printf("3. Multiplication\n");
+  printf("4. Division\n");
+}
