34a5248d2aa70c3cfaa4b5897b1168cd46a900f6
DianaWangui <dianawanguikt048@gmail.com>
2024-02-10 19:44:25 +0300

    update the operation functions

--- operation.c
+++ operation.c
@@ -6,90 +6,132 @@ of the operator and enter the number they want to add*/
 int operation(void)
 {
   int choice;
-  int values;
+  int i;
   int count = 0;
-  int num;
-   int result;
+  int num[5];
+  int result;
 
-  printf("Enter your choice here: ")
+  printf("Enter your choice here: ");
   scanf("%d", &choice);
 
   switch(choice)
   {
     /*addition*/
     case 1:
-      printf("Enter your numbers here, separated by space:");
-      values = malloc((count + 1) * sizeof(int));
-      values[count++] = num;
-      count++;
+      printf("You chose addition.\n");
+      printf("Enter your numbers here:");
+      for (i = 0; i < 5; i++)
+      {
+        if (scanf("%d", &num[i]) != 1)
+        {
+          perror("Error taking your input");
+          return (1);
+        }
+        count++;
+      }
 
       /*This will check if user enters more than 5 operations*/
       if (count > 5)
       {
-        fprintf("Cannot take more than 5 values");
+        fprintf(stderr, "Cannot take more than 5 values\n");
         return (1);
       }
       else 
       {
         /*This is from the function.c file*/
-        result = add(count, num[1], num[2], num[3], num[4]);
+        result = add(count, num[0], num[1], num[2], num[3], num[4]);
+        printf("%d\n", result);
       }
+        break;
 
       /*subtraction*/
       case 2:
-      printf("Enter your numbers here, separated by space:");
-      values = malloc((count + 1) * sizeof(int));
-      values[count++] = num;
-      count++;
+        printf("You chose subtraction.\n");
+        printf("Enter your numbers here: ");
+        for (i = 0; i < 5; i++)
+        {
+          if (scanf("%d", &num[i]) != 1)
+          {
+            perror("Error taking your input");
+            return (1);
+          }
+          count++;
+        }
 
-      /*This will check if user enters more than 5 operations*/
-      if (count > 5)
-      {
-        fprintf("Cannot take more than 5 values");
-        return (1);
-      }
-      else 
-      {
-        /*This is from the function.c file*/
-        result = subtract(count, num[1], num[2], num[3], num[4]);
-      }
+        /*This will check if user enters more than 5 operations*/
+        if (count > 5)
+        {
+          fprintf(stderr, "Cannot take more than 5 values\n");
+          return (1);
+        }
+        else 
+        {
+          /*This is from the function.c file*/
+          result = subtract(count, num[0], num[1], num[2], num[3], num[4]);
+          printf("%d\n", result);
+        }
+          break;
 
       /*multiplication*/
       case 3:
-      printf("Enter your numbers here, separated by space:");
-      values = malloc((count + 1) * sizeof(int));
-      values[count++] = num;
-      count++;
+        printf("You chose multiplication.\n");
+        printf("Enter your numbers here: ");
+        for (i = 0; i < 5; i++)
+        {
+          if (scanf("%d", &num[i]) != 1)
+          {
+            perror("Error taking your input");
+            return (1);
+          }
+          count++;
+        }
 
-      /*This will check if user enters more than 5 operations*/
-      if (count > 5)
-      {
-        fprintf("Cannot take more than 5 values");
-        return (1);
-      }
-      else 
-      {
-        /*This is from the function.c file*/
-        result = add(count, num[1], num[2], num[3], num[4]);
-      }
+        /*This will check if user enters more than 5 operations*/
+        if (count > 5)
+        {
+          fprintf(stderr, "Cannot take more than 5 values\n");
+          return (1);
+        }
+        else 
+        {
+          /*This is from the function.c file*/
+          result = multiply(count, num[0], num[1], num[2], num[3], num[4]);
+          printf("%d\n", result);
+        }
+          break;
 
       /*Division*/
-      case 4:
-      printf("Enter your numbers here, separated by space:");
-      values = malloc((count + 1) * sizeof(int));
-      values[count++] = num;
-      count++;
+      case 5:
+        printf("You chose division.\n");
+        printf("Enter your numbers here:");
+        for (i = 0; i < 5; i++)
+        {
+          if (scanf("%d", &num[i]) != 1)
+          {
+            perror("Error taking your input");
+            return (1);
+          }
+          count++;
+        }
 
-      /*This will check if user enters more than 5 operations*/
-      if (count > 5)
-      {
-        fprintf("Cannot take more than 5 values");
-        return (1);
-      }
-      else 
-      {
-        /*This is from the function.c file*/
-        result = add(count, num[1], num[2], num[3], num[4]);
-      }
+        /*This will check if user enters more than 5 operations*/
+        if (count > 5)
+        {
+          fprintf(stderr, "Cannot take more than 5 values\n");
+          return (1);
+        }
+        else 
+        {
+          /*This is from the function.c file*/
+          result = division(count, num[0], num[1], num[2], num[3], num[4]);
+          printf("%d\n", result);
+        }
+          break;
+
+        default:
+          fprintf(stderr, "Invalid choice\n");
+          return (1);
+
+        return (0);
   }
 }
