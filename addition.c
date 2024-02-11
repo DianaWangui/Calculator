fd3ff4f8cd5e9bbfa1aa48d9280ca3bbf820515a
DianaWangui <dianawanguikt048@gmail.com>
2024-02-11 18:03:14 +0300

    update the comments

--- addition.c
+++ addition.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
-/All functions add , sub, div and mul goes here/
+/*All functions add , sub, div and mul goes here*/
 
 int add(int count, ...)
 {
   int i;
   int result = 0;
-  va_list args; /stores our values/
+  va_list args; /*stores our values*/
 
   va_start(args, count);
 
