8a909c5f71be1ff345f9141316c6673ace2ba2a1
DianaWangui <dianawanguikt048@gmail.com>
2024-02-11 12:26:57 +0300

    update the operation fucntion

--- operation.c
+++ operation.c
@@ -3,10 +3,19 @@
 /*This is the function prompt that allows user to make the choice
 of the operator and enter the number they want to add*/
 
+// int prompt_loop(void)
+// {
+//   printf("Enter '=' to calculate")
+//   printf("Enter your numbers here: ")
+//   while(count < 5 && scanf("%d", &num[count]) == 1)
+//   {
+//     count++;
+//   }
+// }
+
 int operation(void)
 {
   int choice;
-  int i;
   int count = 0;
   int num[5];
   int result;
@@ -19,121 +28,104 @@ int operation(void)
   {
     /*addition*/
     case 1:
-      printf("You chose addition.\n");
-      printf("Enter your numbers here:");
-      
-      for (i = 0; i < 5; i++)
+      printf("Enter '=' after you are done.\n");
+      printf("Enter numbers you want to add here: ");
+
+      while (count < 5 && scanf("%d", &num[count]) == 1)
       {
-        if (scanf("%d", &num[i]) != 1)
-        {
-          perror("Error taking your input");
-          return (1);
-        }
         count++;
       }
-
-      /*This will check if user enters more than 5 operations*/
-      if (count > 5)
+      if (count == 0)
       {
-        fprintf(stderr, "Cannot take more than 5 values\n");
+        fprintf(stderr, "You did not enter any value");
         return (1);
       }
       else 
       {
         /*This is from the function.c file*/
         result = add(count, num[0], num[1], num[2], num[3], num[4]);
-        printf("%d\n", result);
+        printf("Your result is: %d\n", result);
       }
         break;
 
       /*subtraction*/
       case 2:
-        printf("You chose subtraction.\n");
-        printf("Enter your numbers here: ");
-        for (i = 0; i < 5; i++)
+        printf("Enter '=' after you are done.\n");
+        printf("Enter the number you want to subtract from: ");
+        scanf("%d", &result);
+        printf("Enter number(s) you want to subtract here: ");
+
+        while (count <= 4 && scanf("%d", &num[count]) == 1)
         {
-          if (scanf("%d", &num[i]) != 1)
-          {
-            perror("Error taking your input");
-            return (1);
-          }
           count++;
         }
-
-        /*This will check if user enters more than 5 operations*/
-        if (count > 5)
+        if (count == 0)
         {
-          fprintf(stderr, "Cannot take more than 5 values\n");
+          fprintf(stderr, "You did not enter any value\n");
           return (1);
+        }
+        if (count > 4)
+        {
+          fprintf(stderr, "Can only take 4 inputs\n");
         }
         else 
         {
           /*This is from the function.c file*/
-          result = subtract(count, num[0], num[1], num[2], num[3], num[4]);
-          printf("%d\n", result);
+          // printf("COUNTED %d\n", count);
+          result = subtract(count, result, num[0], num[1], num[2], num[3]);
+          printf("Your result is: %d\n", result);
         }
           break;
 
       /*multiplication*/
       case 3:
-        printf("You chose multiplication.\n");
-        printf("Enter your numbers here: ");
-        for (i = 0; i < 5; i++)
+        printf("Enter '=' after you are done.\n");
+        printf("Enter the numbers you want to multiply here:");
+
+        while (count < 5 && scanf("%d", &num[count]) == 1)
         {
-          if (scanf("%d", &num[i]) != 1)
-          {
-            perror("Error taking your input");
-            return (1);
-          }
           count++;
         }
-
-        /*This will check if user enters more than 5 operations*/
-        if (count > 5)
+        if (count == 0)
         {
-          fprintf(stderr, "Cannot take more than 5 values\n");
+          fprintf(stderr, "You did not enter any value");
           return (1);
         }
         else 
         {
           /*This is from the function.c file*/
           result = multiply(count, num[0], num[1], num[2], num[3], num[4]);
-          printf("%d\n", result);
+          printf("Your result is: %d\n", result);
         }
           break;
 
       /*Division*/
       case 4:
-        printf("You chose division.\n");
-        printf("Enter your numbers here:");
-        for (i = 0; i < 5; i++)
+        printf("Enter '=' after you are done.\n");
+        printf("Enter the number you want to divide: ");
+        scanf("%d", &result);
+        printf("Enter the number(s) you want to divide with here:");
+
+        while (count <= 4 && scanf("%d", &num[count]) == 1)
         {
-          if (scanf("%d", &num[i]) != 1)
-          {
-            perror("Error taking your input");
-            return (1);
-          }
           count++;
         }
-
-        /*This will check if user enters more than 5 operations*/
-        if (count > 5)
+        if (count == 0)
         {
-          fprintf(stderr, "Cannot take more than 5 values\n");
+          fprintf(stderr, "You did not enter any value");
           return (1);
         }
         else 
         {
           /*This is from the function.c file*/
-          result = division(count, num[0], num[1], num[2], num[3], num[4]);
-          printf("%d\n", result);
+          // printf("%d", count);
+          result = division(count, result, num[0], num[1], num[2], num[3]);
+          printf("Your result is: %d\n", result);
         }
           break;
 
         default:
           fprintf(stderr, "Invalid choice\n");
           return (1);
-
-        return (0);
   }
 }
